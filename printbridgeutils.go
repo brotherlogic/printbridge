fafeb5ca0f95b2f4c86f8fefb64c87db15df8a91
Simon Tucker <brotherlogic@gmail.com>
2021-03-09 18:18:14 -0800

    Parameterize the time since

--- printbridgeutils.go
+++ printbridgeutils.go
@@ -4,16 +4,53 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
+	"log"
+	"strconv"
 	"time"
 
 	"github.com/brotherlogic/goserver/utils"
+	kmpb "github.com/brotherlogic/keymapper/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
 
 	pb "github.com/brotherlogic/hometaskqueue/proto"
 )
 
+func (s *Server) getLastRun() (int64, error) {
+	ctx, cancel := utils.ManualContext("ghc", "ghc", time.Minute, false)
+	conn, err := s.FDialServer(ctx, "keymapper")
+	if err != nil {
+		if status.Convert(err).Code() == codes.Unknown {
+			log.Fatalf("Cannot reach keymapper: %v", err)
+		}
+		return -1, err
+	}
+	client := kmpb.NewKeymapperServiceClient(conn)
+	resp, err := client.Get(ctx, &kmpb.GetRequest{Key: "hometaskqueue_last"})
+	if err != nil {
+		if status.Convert(err).Code() == codes.Unknown || status.Convert(err).Code() == codes.InvalidArgument {
+			log.Fatalf("Cannot read external: %v", err)
+		}
+		return -1, err
+	}
+	cancel()
+
+	val, err := strconv.ParseInt(resp.Key.GetValue(), 10, 64)
+	if err != nil {
+		return -1, err
+	}
+
+	return val, nil
+}
+
 func (s *Server) runLoop() error {
+	val, err := s.getLastRun()
+	if err != nil {
+		return err
+	}
+
 	ctx, cancel := utils.ManualContext("pbu", "pbu", time.Minute, false)
 	defer cancel()
 
@@ -38,6 +75,7 @@ func (s *Server) runLoop() error {
 
 	res, err := client.GetTasks(ctx, &pb.GetTasksRequest{
 		QueueId: s.key,
+		Since:   val,
 	})
 
 	if err != nil {
